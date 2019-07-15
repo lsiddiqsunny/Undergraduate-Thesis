b9ecc4bd20c08e52e99ad80959c58dbb99f9903f
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2019-07-16 01:26:50 +0600

    Update work with javaparser dumped into json

--- Java_parser/src/Main.java
+++ Java_parser/src/Main.java
@@ -1,33 +1,28 @@
 
 
 import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseException;
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.google.common.base.Strings;
+import com.github.javaparser.printer.JsonPrinter;
+import com.github.javaparser.printer.YamlPrinter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Pattern;
+
+import java.io.*;
 
 public class Main {
 
-    private static final String FILE_PATH = "D:\\Thesis\\Java parser\\src\\Main.java";
+    private static final String FILE_PATH = "D:\\Thesis\\Undergraduate-Thesis\\Java_parser\\src\\test.java";
 
     public static void listClasses(File projectDir) {
         new Director((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
             System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
             try {
                 new VoidVisitorAdapter<Object>() {
                     @Override
@@ -35,7 +30,7 @@ public class Main {
                         super.visit(n, arg);
                         System.out.println(" * " + n.getName());
                     }
-                }.visit(StaticJavaParser.parse(file), null);
+                }.visit(JavaParser.parse(file), null);
                 System.out.println(); // empty line
             } catch (IOException e) {
                 new RuntimeException(e);
@@ -45,9 +40,9 @@ public class Main {
 
 
     public static void listMethodCalls(File projectDir) {
-        new Director((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+       new Director((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+
             System.out.println(path);
-            System.out.println(Strings.repeat("=", path.length()));
             try {
                 new VoidVisitorAdapter<Object>() {
                     @Override
@@ -55,23 +50,83 @@ public class Main {
                         super.visit(n, arg);
                         System.out.println(" [L " + n.getName() + "] " + n);
                     }
-                }.visit(StaticJavaParser.parse(file), null);
+                }.visit(JavaParser.parse(file), null);
                 System.out.println(); // empty line
             } catch (IOException e) {
                 new RuntimeException(e);
             }
         }).explore(projectDir);
     }
-    public static void main(String[] args) throws FileNotFoundException {
-        CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
-        List<Comment> comments = cu.getAllContainedComments();
+    public static String toPrettyFormat(String jsonString)
+    {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(jsonString).getAsJsonObject();
 
-        Iterator iterator = comments.iterator();
-        while(iterator.hasNext()) {
-            System.out.println(iterator.next().toString());
-        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String prettyJson = gson.toJson(json);
 
-        File projectDir = new File("D:\\Thesis\\Java parser\\src");
-        listMethodCalls(projectDir);
+        return prettyJson;
+    }
+
+
+    public static void listMethodDeclaration(File projectDir) throws FileNotFoundException, UnsupportedEncodingException {
+        JavaParser.getStaticConfiguration().setAttributeComments(false);
+
+        PrintWriter writer = new PrintWriter("testout.json", "UTF-8");
+        new Director((level, path, file) -> path.endsWith(".txt"), (level, path, file) -> {
+
+            System.out.println(path);
+            //System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(MethodDeclaration n, Object arg) {
+                        super.visit(n, arg);
+                      //  System.out.println(n + "\n");
+                        YamlPrinter printer = new YamlPrinter(true);
+                        JsonPrinter jprinter=new  JsonPrinter(true);
+
+                        // System.out.println(printer.output(n));
+
+                        writer.println(toPrettyFormat(jprinter.output(n)));
+                       String data=jprinter.output(n);
+//
+                      //  System.out.println(data);
+                   //   System.out.println(toPrettyFormat(data));
+
+
+
+                    }
+                }.visit(JavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (RuntimeException e) {
+                new RuntimeException(e);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }).explore(projectDir);
+        writer.close();
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+      //  CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
+      ///  System.out.println(cu);
+
+
+    // Now comes the inspection code:
+        YamlPrinter printer = new YamlPrinter(true);
+       // System.out.println(printer.output(cu));
+
+       // List<Comment> comments = cu.getAllContainedComments();
+        //System.out.println("******\n"+cu.toString()+"\n******");
+
+       // Iterator iterator = comments.iterator();
+        //while(iterator.hasNext()) {
+            //System.out.println(iterator.next().toString());
+        //}
+
+        File projectDir = new File("D:\\Thesis\\Undergraduate-Thesis\\Java_parser");
+        listMethodDeclaration(projectDir);
     }
 }
