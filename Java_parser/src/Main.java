8cddb7bcffa7e0d5b3b2a16f3d23b219e42dccca
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2019-07-24 01:36:50 +0600

    update parser

--- Java_parser/src/Main.java
+++ Java_parser/src/Main.java
@@ -72,7 +72,8 @@ public class Main {
     public static void listMethodDeclaration(File projectDir) throws FileNotFoundException, UnsupportedEncodingException {
         JavaParser.getStaticConfiguration().setAttributeComments(false);
 
-        PrintWriter writer = new PrintWriter("outfrom2852019.json", "UTF-8");
+        PrintWriter writer1 = new PrintWriter("outjsonfrom2652019.json", "UTF-8");
+        PrintWriter writer2 = new PrintWriter("outjavafrom2652019.java", "UTF-8");
         new Director((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
 
             System.out.println(path);
@@ -85,8 +86,8 @@ public class Main {
                       //  System.out.println(n + "\n");
                         YamlPrinter printer = new YamlPrinter(true);
                         JsonPrinter jprinter=new  JsonPrinter(true);
-                        writer.println(toPrettyFormat(jprinter.output(n))+"\n\n######\n\n");
-
+                        writer1.println(toPrettyFormat(jprinter.output(n))+"\n\n######\n\n");
+                        writer2.println(n.toString()+"\n\n######\n\n");
                         // System.out.println(printer.output(n));
 
                       //  writer.println(toPrettyFormat(jprinter.output(n))+"\n#######\n");
@@ -106,11 +107,12 @@ public class Main {
                 e.printStackTrace();
             }
         }).explore(projectDir);
-        writer.close();
+        writer1.close();
+        writer2.close();
     }
     public static void Parse() throws IOException {
 
-        InputStream is = new FileInputStream("D:\\Thesis\\Undergraduate-Thesis\\Java_parser\\outfrom2852019.json");
+        InputStream is = new FileInputStream("D:\\Thesis\\Undergraduate-Thesis\\Java_parser\\outjsonfrom2652019.json");
         BufferedReader buf = new BufferedReader(new InputStreamReader(is));
         String line = buf.readLine();
         StringBuilder sb = new StringBuilder();
@@ -119,13 +121,46 @@ public class Main {
 
         String [] data=fileAsString.split("######");
 
+
+        InputStream is1 = new FileInputStream("D:\\Thesis\\Undergraduate-Thesis\\Java_parser\\outjavafrom2652019.java");
+        BufferedReader buf1 = new BufferedReader(new InputStreamReader(is1));
+        String line1 = buf1.readLine();
+        StringBuilder sb1 = new StringBuilder();
+        while(line1 != null){ sb1.append(line1).append("\n"); line1 = buf1.readLine(); }
+        String fileAsString1 = sb1.toString();
+
+        String [] data1=fileAsString1.split("######");
+
         int i=0;
         String dir="D:\\Thesis\\Mined\\Output_from_28_5_2019\\28_5_2019_";
         String ext=".json";
+
         for(String s: data){
-            PrintWriter writer = new PrintWriter(dir+Integer.toString(i+1)+ext, "UTF-8");
+            File directory = new File(dir+Integer.toString(i+1));
+            if (! directory.exists()){
+                directory.mkdir();
+
+            }
+
+
+            PrintWriter writer = new PrintWriter(dir+Integer.toString(i+1)+"\\ast"+ext, "UTF-8");
             writer.println(s);
             writer.close();
+
+
+            i++;
+        }
+
+        i=0;
+        String dir1="D:\\Thesis\\Mined\\Output_from_28_5_2019\\28_5_2019_";
+        String ext1=".java";
+
+        for(String s: data1){
+            PrintWriter writer = new PrintWriter(dir1+Integer.toString(i+1)+"\\code"+ext1, "UTF-8");
+            writer.println(s);
+            writer.close();
+
+
             i++;
         }
 
@@ -152,7 +187,7 @@ public class Main {
             //System.out.println(iterator.next().toString());
         //}
 
-       // File projectDir = new File("D:\\Thesis\\Mined\\output from  28-5-2019");
+     //   File projectDir = new File("D:\\Thesis\\Mined\\output from  28-5-2019");
      //   listMethodDeclaration(projectDir);
         Parse();
     }
