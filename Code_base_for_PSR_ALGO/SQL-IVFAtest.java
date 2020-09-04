d303f2931fb7bc87adbaeade5037fc92f5ad40e0
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2020-09-04 06:22:35 +0600

    Updated

--- Code_base_for_PSR_ALGO/SQL-IVFAtest.java
+++ Code_base_for_PSR_ALGO/SQL-IVFAtest.java
@@ -1,35 +1,40 @@
 import java.sql.Statement;
 
-public class dummy{
-private static java.util.Vector traverseInputTree(java.util.Vector vectorTree, java.util.Vector returnVector){
-for(int i = 0; i < vectorTree.size(); i++){
-if (vectorTree.get(i) instanceof java.util.Vector) {
-java.util.Vector nextVectorNode = (java.util.Vector) vectorTree.get(i);
-traverseInputTree(nextVectorNode, returnVector);
-}
-else {
-returnVector.add(vectorTree.get(i));
-}
-}
-return returnVector;
-}
-    public void f(){
-        Connection conn  = connect();
-        Statement stmt  = conn.createStatement();
- String PSqueryUniqueID0;
- java.util.Vector PSInput00 = new java.util.Vector();
-{
-PSInput00 = new java.util.Vector();
-PSInput00.add(a);
-PSqueryUniqueID0 = "select * from table where id = ""?";
-}
-java.util.Vector returnVector0 = new java.util.Vector();
-traverseInputTree(PSInput00, returnVector0);
-java.sql.PreparedStatement ps0 = stmt.getConnection().prepareStatement(PSqueryUniqueID0);
-for(int i = 0; i < returnVector0.size(); i++){
-ps0.setObject((i + 1), returnVector0.get(i));
-}
-        ps0.execute();
- 
+public class dummy {
+
+  private static java.util.Vector traverseInputTree(
+    java.util.Vector vectorTree,
+    java.util.Vector returnVector
+  ) {
+    for (int i = 0; i < vectorTree.size(); i++) {
+      if (vectorTree.get(i) instanceof java.util.Vector) {
+        java.util.Vector nextVectorNode = (java.util.Vector) vectorTree.get(i);
+        traverseInputTree(nextVectorNode, returnVector);
+      } else {
+        returnVector.add(vectorTree.get(i));
+      }
     }
+    return returnVector;
+  }
+
+  public void f() {
+    Connection conn = connect();
+    Statement stmt = conn.createStatement();
+    String PSqueryUniqueID0;
+    java.util.Vector PSInput00 = new java.util.Vector();
+    {
+      PSInput00 = new java.util.Vector();
+      PSInput00.add(a);
+      PSqueryUniqueID0 = "select * from table where id = " + "?";
+    }
+    java.util.Vector returnVector0 = new java.util.Vector();
+    traverseInputTree(PSInput00, returnVector0);
+    java.sql.PreparedStatement ps0 = stmt
+      .getConnection()
+      .prepareStatement(PSqueryUniqueID0);
+    for (int i = 0; i < returnVector0.size(); i++) {
+      ps0.setObject((i + 1), returnVector0.get(i));
+    }
+    ps0.execute();
+  }
 }
--- Code_base_for_PSR_ALGO/runner.py
+++ Code_base_for_PSR_ALGO/runner.py
@@ -1,13 +1,15 @@
 import os
 import csv
-file_arr = os.listdir('./Test_Edit Trees')
+file_arr = os.listdir('.\Test_Edit Trees')
 with open('linenumber.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for file in file_arr:
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java', 'r+') as f:
-            content = f.read()
+        # with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java', 'r+') as f:
+        #     content = f.read()
+            # f.seek(0, 0)
+            # f.write('public '+content)
         linenum = ''
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:
             print(' IN '+file[7:-5]+" : ")
             lookup = 'select '
             for num, line in enumerate(myFile, 1):
@@ -15,19 +17,19 @@ with open('linenumber.csv', 'w', newline='') as file:
                     linenum+=str(num)+' '
                     print ('found at line:', num)
 
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:
             lookup = 'insert '
             for num, line in enumerate(myFile, 1):
                 if lookup.lower() in line.lower():
                     linenum+=str(num)+' '
                     print ('found at line:', num)
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:                   
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:                   
             lookup = 'delete '
             for num, line in enumerate(myFile, 1):
                 if lookup.lower() in line.lower():
                     linenum+=str(num)+' '
                     print ('found at line:', num)
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:
 
             lookup = 'update '
             for num, line in enumerate(myFile, 1):
@@ -35,21 +37,21 @@ with open('linenumber.csv', 'w', newline='') as file:
                     linenum+=str(num)+' '
                     print ('found at line:', num)
 
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:
 
             lookup = 'statement'
             for num, line in enumerate(myFile, 1):
                 if lookup.lower() in line.lower():
                     linenum+=str(num)+' '
                     print ('found at line:', num)
-        with open('/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_'+file[7:-5]+'.java','r') as myFile:
+        with open('D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_'+file[7:-5]+'.java','r') as myFile:
 
             lookup = 'execute'
             for num, line in enumerate(myFile, 1):
                 if lookup.lower() in line.lower():
                     linenum+=str(num)+' '
                     print ('found at line:', num)
-        os.system('java Fixer '+'"/media/lsiddiqsunny/New Volume/Thesis/Getafix/gumtree-spoon-ast-diff+clustering/Before/before_"'+file[7:-5]+'.java '+linenum) 
+        os.system('java Fixer '+'"D:\\Thesis\\Getafix\\gumtree-spoon-ast-diff+clustering\\Before\\before_"'+file[7:-5]+'.java '+linenum) 
         writer.writerow([file[7:-5] , linenum])
 
 
